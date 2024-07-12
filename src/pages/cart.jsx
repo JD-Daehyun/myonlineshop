62d08bbb26677a804076b5e394f928a41311a2c7
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-11 19:39:37 -0700

    Add useSelector to utilize the payload. Also, create total and use reduce function to keep track of the total amount

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
 
 
 
 
 export default function Cart(){
+    const [totalCart, setTotalCart] = useState(0);
 
+
+    const { cart } = useSelector(state => state)
+
+    useEffect(()=>{
+        //reduce function to combine the total amount!
+        setTotalCart(cart.reduce((acc,curr) => acc + curr.price, 0));
+    },[cart])
     return <div>Cart</div>
 }
